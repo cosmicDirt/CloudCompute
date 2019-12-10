3ec85c715f732fd829bea68311d46d10c7271801
ye <549127129@qq.com>
2019-12-10 10:27:16 +0800

    init project

---
+++ src/main/java/com/example/cloudcompute/CloudcomputeApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudcompute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudcomputeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudcomputeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cloudcompute/CloudcomputeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudcompute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudcomputeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
