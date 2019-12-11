6d78b9feffbb3343ee18fcfdaf44b021a3af462c
ye <549127129@qq.com>
2019-12-11 17:26:24 +0800

    merge

--- src/main/java/com/example/cloudcompute/service/CloudcomputeApplication.java
+++ src/main/java/com/example/cloudcompute/service/CloudcomputeApplication.java
@@ -1,4 +1,4 @@
-package com.example.cloudcompute;
+package com.example.cloudcompute.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/example/cloudcompute/service/ControllerRouter.java
@@ -0,0 +1,27 @@
+package com.example.cloudcompute.service;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class ControllerRouter {
+    @GetMapping("/")
+    public String getIndexPage()
+    {
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage()
+    {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String getRegisterPage()
+    {
+        return "register";
+    }
+}
